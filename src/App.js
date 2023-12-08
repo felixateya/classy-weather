d184b4ec6c128d3e002e2df47c1cebc2aeab10d5
felixateya <mulaafelix6@gmail.com>
2023-12-08 15:44:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], "☀️"],
+    [[1], "🌤"],
+    [[2], "⛅️"],
+    [[3], "☁️"],
+    [[45, 48], "🌫"],
+    [[51, 56, 61, 66, 80], "🌦"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
+    [[71, 73, 75, 77, 85, 86], "🌨"],
+    [[95], "🌩"],
+    [[96, 99], "⛈"],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return "NOT FOUND";
+  return icons.get(arr);
 }
 
+function convertToFlag(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
+function formatDay(dateStr) {
+  return new Intl.DateTimeFormat("en", {
+    weekday: "short",
+  }).format(new Date(dateStr));
+}
+
+const App = () => {
+  const [location, setLocation] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [displayLocation, setDisplayLocation] = useState("");
+  const [weather, setWeather] = useState({});
+
+  // const fetchWeather = async () => {
+  //   if (location.length < 2) return setWeather({});
+  //   try {
+  //     setIsLoading(true);
+  //     // 1. Getting location (geocoding)
+  //     const geoRes = await fetch(
+  //       `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+  //     );
+  //     const geoData = await geoRes.json();
+  //     console.log(geoData);
+
+  //     if (!geoData.results) throw new Error("Location not found");
+
+  //     const { latitude, longitude, timezone, name, country_code } =
+  //       geoData.results.at(0);
+
+  //     setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
+
+  //     // 2) Getting actual weather
+  //     const weatherRes = await fetch(
+  //       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+  //     );
+  //     const weatherData = await weatherRes.json();
+
+  //     setWeather(weatherData.daily);
+  //   } catch (err) {
+  //     console.error(err);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  const setMyLocation = (e) => setLocation(e.target.value);
+
+  useEffect(() => {
+    setLocation(localStorage.getItem("location" || ""));
+  }, []);
+  useEffect(
+    (prevProps, prevState) => {
+      // if (location !== prevState?.location) fetchWeather();
+      if (location !== prevState?.location) {
+        const fetchWeather = async () => {
+          if (location.length < 2) return setWeather({});
+          try {
+            setIsLoading(true);
+            // 1. Getting location (geocoding)
+            const geoRes = await fetch(
+              `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+            );
+            const geoData = await geoRes.json();
+            console.log(geoData);
+      
+            if (!geoData.results) throw new Error("Location not found");
+      
+            const { latitude, longitude, timezone, name, country_code } =
+              geoData.results.at(0);
+      
+            setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
+      
+            // 2) Getting actual weather
+            const weatherRes = await fetch(
+              `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+            );
+            const weatherData = await weatherRes.json();
+      
+            setWeather(weatherData.daily);
+          } catch (err) {
+            console.error(err);
+          } finally {
+            setIsLoading(false);
+          }
+        };
+        fetchWeather()
+      }
+      localStorage.setItem("location", location);
+    },
+    [location]
+  );
+
+  return (
+    <div className="app">
+      <h1>Classy Weather</h1>
+      <Input location={location} onChangeLocation={setMyLocation} />
+      {isLoading && <p className="loader">Loading...</p>}
+      {weather.weathercode && (
+        <Weather weather={weather} location={displayLocation} />
+      )}
+    </div>
+  );
+};
+
 export default App;
+
+const Input = ({ location, onChangeLocation }) => {
+  return (
+    <input
+      type="text"
+      placeholder="search from location..."
+      value={location}
+      onChange={onChangeLocation}
+    />
+  );
+};
+
+const Weather = ({ weather, location }) => {
+  const {
+    temperature_2m_max: max,
+    temperature_2m_min: min,
+    time: dates,
+    weathercode: codes,
+  } = weather;
+  return (
+    <div>
+      <h2>Weather {location}</h2>
+      <ul className="weather">
+        {dates.map((date, index) => (
+          <Day
+            date={date}
+            max={max.at(index)}
+            min={min.at(index)}
+            code={codes.at(index)}
+            key={date}
+            isToday={index === 0}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Day = ({ date, max, min, code, isToday }) => {
+  return (
+    <li className="day">
+      <span>{getWeatherIcon(code)}</span>
+      <p>{isToday ? "Today" : formatDay(date)}</p>
+      <p>
+        {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+      </p>
+    </li>
+  );
+};
